079bea225c1b68e7818908b36baabcb33b7fb8a0
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-21 19:24:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,27 +16,13 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-board = ["X", "O", "X", "O", "X", "O", "X", "X", "O"]
-
 def won?(board)
-  index_iterator = [0,1,2,3,4,5,6,7,8]
-  x = Array.new
-  o = Array.new
-  index_iterator.each do |index|
-     if board[index] == "X"
-      x << index
-     elsif board[index] == "O"
-      o << index
-     end
-     x
-     o
-   end   
+  WIN_COMBINATIONS.eacho do |win_combination|
+
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      return win_combination
+    else
+      false
+    end
+  end
 end
-   WIN_COMBINATIONS.detect do |win|
-     if win == x || o
-       win
-     else
-       false
-     end
-   end
- end
