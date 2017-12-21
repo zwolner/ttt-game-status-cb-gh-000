0ba7b8a0df94cfef835968ce35e0eb4b5a75ffcf
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-21 19:03:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,3 +15,22 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+index_iterator = [0,1,2,3,4,5,6,7,8]
+x = Array.new
+o = Array.new
+def won?(board)
+  index_iterator.each do |index|
+     if board[index] == "X"
+       x << index
+     else board[index] == "O"
+       o << index
+     end
+   end
+   WIN_COMBINATIONS.detect do |win|
+     if win == "X" || "O"
+       win
+     else
+       false
+     end
+   end
+ end
