79544ea7056c363ed3e31e5316efbad400866ea2
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-21 19:25:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,7 @@ def won?(board)
 
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
       return win_combination
+    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
     else
       false
     end
