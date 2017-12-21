2bc58cbfdab88c2717768102f4c27c59c05cc906
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-21 19:38:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.select do |win_combination|
-    return true if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"  
-    return true if [win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+    return win_combination if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"  
+    return win_combination if [win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
   end
 end
