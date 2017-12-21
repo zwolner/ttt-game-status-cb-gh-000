0648a2c8a597533f987212fbc71d9417af0df878
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-21 18:43:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,12 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+def WIN_COMBINATIONS = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,7]
+  [0,3,6]
+  [1,4,7]
+  [0,4,7]
+  [2,4,6]
+]
